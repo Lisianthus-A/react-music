64f6ad77b0a7a1a71b6c277b13473d45cd826b4e
lisianthus-a <1419033252@qq.com>
2020-10-19 18:04:43 +0800

    Optimize Toggle Button

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,4 +1,4 @@
-import React, { Suspense, useState, useRef } from 'react';
+import React, { Suspense } from 'react';
 import './index.scss';
 import Loading from '../Loading';
 import Header from '../Header';
@@ -7,41 +7,28 @@ import MusicPlayer from '../MusicPlayer';
 
 const Layout = (props) => {
     const { TargetComponent } = props;
-    const [visible, setVisible] = useState(true);
-    const layoutRef = useRef(null);
-    const sidebarRef = useRef(null);
-    const arrowRef = useRef(null);
-
-    const toggleVisible = () => {
-        if (visible) {
-            layoutRef.current.classList.add('layout-change');
-            arrowRef.current.classList.add('rotate');
-            sidebarRef.current.style.display = 'none';
-        } else {
-            layoutRef.current.classList.remove('layout-change');
-            arrowRef.current.classList.remove('rotate');
-            sidebarRef.current.style.display = 'block';
-        }
-        setVisible(!visible);
-    }
 
     return (
-        <div className="layout" ref={layoutRef}>
+        <div className="layout">
             <div className="top">
                 <Header />
             </div>
 
-            <div className="middle-left">
-                <Sidebar ref={sidebarRef} />
-                <div className='toggle-button' onClick={toggleVisible}>
-                    <span className='arrow' ref={arrowRef}></span>
+            <div className='middle'>
+                <input type="checkbox" id="toggle" style={{ display: "none" }} />
+                <div className="left">
+                    <Sidebar />
+                    <label htmlFor="toggle">
+                        <div className='toggle-button'>
+                            <span className='arrow' ></span>
+                        </div>
+                    </label>
                 </div>
-            </div>
-
-            <div className="middle-right">
-                <Suspense fallback={<Loading />}>
-                    <TargetComponent />
-                </Suspense>
+                <div className="right">
+                    <Suspense fallback={<Loading />}>
+                        <TargetComponent />
+                    </Suspense>
+                </div>
             </div>
 
             <div className="bottom">
