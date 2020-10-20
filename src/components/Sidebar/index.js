c9292faec96d596f4d6940fcccef1b2a45cfdeca
Lisianthus-A <lisianthus.zeng@gmail.com>
2020-10-20 22:59:23 +0800

    optimize and fix bug

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -13,40 +13,48 @@ const Sidebar = forwardRef((props, ref) => {
     }
 
     return (
-        <div className='sidebar' ref={ref}>
-            <div className='category'>推荐</div>
-            {
-                [
-                    { text: '发现音乐', route: '/Discovery', Icon: SearchOutlined, key: 0 },
-                    { text: '私人FM', route: '/Discovery', Icon: CustomerServiceOutlined, key: 1 },
-                    { text: '视频', route: '/Discovery', Icon: VideoCameraOutlined, key: 2 }
-                ].map(
-                    e =>
-                        <div
-                            className={currentKey === e.key ? 'item active' : 'item'}
-                            key={e.key}
-                            onClick={() => handleClick(e.key, e.route)}
-                        >
-                            <e.Icon /> {e.text}
-                        </div>
-                )
-            }
-            <div className='category'>我的音乐</div>
-            {
-                [
-                    { text: '我的歌单', route: '/Discovery', Icon: HeartOutlined, key: 3 }
-                ].map(
-                    e =>
-                        <div
-                            className={currentKey === e.key ? 'item active' : 'item'}
-                            key={e.key}
-                            onClick={() => handleClick(e.key, e.route)}
-                        >
-                            <e.Icon /> {e.text}
-                        </div>
-                )
-            }
-        </div>
+        <>
+            <input type='checkbox' id='toggle' style={{ display: 'none' }} />
+            <div className='sidebar' ref={ref}>
+                <div className='category'>推荐</div>
+                {
+                    [
+                        { text: '发现音乐', route: '/Discovery', Icon: SearchOutlined, key: 0 },
+                        { text: '私人FM', route: '/Discovery', Icon: CustomerServiceOutlined, key: 1 },
+                        { text: '视频', route: '/Discovery', Icon: VideoCameraOutlined, key: 2 }
+                    ].map(
+                        e =>
+                            <div
+                                className={currentKey === e.key ? 'item active' : 'item'}
+                                key={e.key}
+                                onClick={() => handleClick(e.key, e.route)}
+                            >
+                                <e.Icon /> {e.text}
+                            </div>
+                    )
+                }
+                <div className='category'>我的音乐</div>
+                {
+                    [
+                        { text: '我的歌单', route: '/Discovery', Icon: HeartOutlined, key: 3 }
+                    ].map(
+                        e =>
+                            <div
+                                className={currentKey === e.key ? 'item active' : 'item'}
+                                key={e.key}
+                                onClick={() => handleClick(e.key, e.route)}
+                            >
+                                <e.Icon /> {e.text}
+                            </div>
+                    )
+                }
+            </div>
+            <label htmlFor="toggle">
+                <div className='toggle-button'>
+                    <span className='arrow' ></span>
+                </div>
+            </label>
+        </>
     );
 });
 
