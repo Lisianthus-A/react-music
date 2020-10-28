ebf96c9726177a5be46c400d4ed6d898a13848f6
lisianthus-a <1419033252@qq.com>
2020-10-28 14:13:36 +0800

    fix progressBar

--- src/components/MusicPlayer/ProgressBar/index.js
+++ src/components/MusicPlayer/ProgressBar/index.js
@@ -1,49 +1,63 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useRef } from 'react';
 import './index.scss';
-import { Slider } from 'antd';
 import { convertTime } from 'Utils';
 
-const ProgressBar = ({ audioRef, current, setTime, duration }) => {
-    /*
-        ant Slider有问题，以后重构
-     
-    const [value, setValue] = useState(null);
+const ProgressBar = ({ audioRef, current, duration }) => {
     const [visible, setVisible] = useState(false);  //文本框是否可见
+    const railRef = useRef(null);
+    const tipRef = useRef(null);
 
-    const handleChange = useCallback((value) => {
-        setValue(value);  //使Slider展示value的值
+    //鼠标按下
+    const handleMouseDown = useCallback((e) => {
+        window.addEventListener('mousemove', handleMouseMove);
+        window.addEventListener('mouseup', handleMouseUp);
+
         setVisible(true);
-        console.log('change', value);
+        const percent = e.clientX / window.innerWidth;
+        railRef.current.style.width = `${percent * 100}%`;
+        tipRef.current.innerText = convertTime(percent * duration);
     },
         []
     );
 
-    const handleAfterChange = (value) => {
-        console.log('after');
-        const percent = value / 10000;
-        audioRef.current.currentTime = percent * duration;
-        setTime(percent * duration);
+    //鼠标抬起
+    const handleMouseUp = useCallback((e) => {
+        window.removeEventListener('mousemove', handleMouseMove);
+        window.removeEventListener('mouseup', handleMouseUp);
+        
         setVisible(false);
-        setValue(null);
-    }
+        const percent = e.clientX / window.innerWidth;
+        audioRef.current.currentTime = percent * duration;
+    },
+        []
+    );
 
-    //文本框显示的文本
-    const tipFormatter = (value) => {
-        const percent = value / 10000;
-        return convertTime(percent * duration);
-    }
-    */
+    //鼠标移动
+    const handleMouseMove = useCallback((e) => {
+        const percent = e.clientX / window.innerWidth;
+        railRef.current.style.width = `${percent * 100}%`;
+        tipRef.current.innerText = convertTime(percent * duration);
+    },
+        []
+    );
+
+    const getStyle = useCallback(() => {
+        if (visible) {  //文本框可见，说明正在拖动
+            return { width: railRef.current.style.width };
+        } else {
+            return { width: `${current / duration * 100}%` };
+        }
+    },
+        [current, duration]
+    );
 
     return (
         <div className='progressbar'>
-            <Slider
-                value={current / duration * 10000}
-                max={10000}
-                // onChange={handleChange}
-                // onAfterChange={handleAfterChange}
-                // tipFormatter={tipFormatter}
-                tooltipVisible={false}
-            />
+            <div className='track' onMouseDown={handleMouseDown}></div>
+            <div className='rail' onMouseDown={handleMouseDown} ref={railRef} style={getStyle()}>
+                <div className='handle'></div>
+                <div className='tooltip' ref={tipRef} style={{ display: visible ? 'block' : 'none' }}>00:00</div>
+            </div>
         </div>
     );
 }
