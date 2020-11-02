ba561a639f4d4a162f59bf61b10773ba785db03d
Lisianthus-A <lisianthus.zeng@gmail.com>
2020-11-02 17:46:42 +0800

    add utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -19,11 +19,8 @@ export const convertTime = (time) => {
     return `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
 }
 
-//将播放次数转换成形如 x万 x亿 的字符串，不满1万则原样返回
+//将播放次数转换成形如 x万 的字符串，不满1万则原样返回
 export const convertCount = (count) => {
-    if (count >= 100000000) {
-        return `${parseInt(count / 100000000)}亿`;
-    }
     if (count >= 10000) {
         return `${parseInt(count / 10000)}万`;
     }
@@ -45,4 +42,15 @@ export const searchItem = (search, item) => {
 export const convertTimeString = (str) => {
     const arr = str.split(':');
     return arr[0] * 60 + +arr[1];
+}
+
+//将时间戳转换成形如刚刚、x分钟前、x小时前、x天前、x月前、x年前的字符串
+export const convertDate = (timestamp) => {
+    const diff = Date.now() - new Date(timestamp);
+    const d = new Date(diff);
+    return diff <= 60000 ? '刚刚' :  //60秒内
+        diff <= 3600000 ? `${parseInt(diff / 60000)}分钟前` :  //60分钟内
+        diff <= 86400000 ? `${parseInt(diff / 3600000)}小时前` :  //24小时内
+        d.getFullYear() > 1970 ? `${d.getFullYear() - 1970}年前` :
+        d.getMonth() ? `${d.getMonth()}个月前` : `${d.getDate() - 1}天前`;
 }
