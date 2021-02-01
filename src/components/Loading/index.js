9b6a3cd0202509539f9a86de0f2395298dbe5e6b
lisianthus-a <1419033252@qq.com>
2021-02-01 14:54:58 +0800

    pref: Optimize the experience

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import style from './index.module.scss';
 
+const array = new Array(16).fill(0);
+
 const Loading = () => {
     return (
-        <div className={style.loading}></div>
+        <div className={style.container}>
+            <div className={style.loader}>
+                {array.map((_, idx) =>
+                    <div className={style.square} key={idx} />
+                )}
+            </div>
+            <div className="loading-text">加载中</div>
+        </div>
     );
 }
 
