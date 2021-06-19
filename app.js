8fac809e23d55ed2c617ec18edf1ca0371d8a777
Lisianthus-A <lisianthus.zeng@gmail.com>
2021-06-19 13:21:29 +0800

    update

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   if (req.path !== '/' && !req.path.includes('.')) {
     res.set({
       'Access-Control-Allow-Credentials': true,
-      'Access-Control-Allow-Origin': 'https://lisianthus-a.github.io',
+      'Access-Control-Allow-Origin': req.headers.origin || '*',
       'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
       'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
       'Content-Type': 'application/json; charset=utf-8',
