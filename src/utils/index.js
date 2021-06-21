6b4f16e03ffe25989fef680d05ed96e7940eb9fb
Lisianthus-A <lisianthus.zeng@gmail.com>
2021-06-21 22:50:06 +0800

    pref: optimize

--- src/utils/index.js
+++ src/utils/index.js
@@ -29,8 +29,8 @@ export const convertDate = (timestamp) => {
     const diff = Date.now() - new Date(timestamp);
     const d = new Date(diff);
     return diff <= 60000 ? '刚刚' :  //60秒内
-        diff <= 3600000 ? `${parseInt(diff / 60000)}分钟前` :  //60分钟内
-        diff <= 86400000 ? `${parseInt(diff / 3600000)}小时前` :  //24小时内
+        diff <= 3600000 ? `${diff / 60000 >> 0}分钟前` :  //60分钟内
+        diff <= 86400000 ? `${diff / 3600000 >> 0}小时前` :  //24小时内
         d.getFullYear() > 1970 ? `${d.getFullYear() - 1970}年前` :
         d.getMonth() ? `${d.getMonth()}个月前` : `${d.getDate() - 1}天前`;
 }
