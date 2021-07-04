b8892a121303f9877982bc0d73075c8e3a78b9d9
Lisianthus-A <lisianthus.zeng@gmail.com>
2021-07-05 00:41:31 +0800

    feat: Encapsulate the Tabs component

--- src/routes/Singer/components/View.js
+++ src/routes/Singer/components/View.js
@@ -1,33 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 import style from './View.module.scss';
 
 import Loading from 'Components/Loading';
 import SongList from 'Components/SongList';
 import Header from './Header';
-import Tabs from './Tabs';
+import Tabs from 'Components/Tabs';
 import Introduction from './Introduction';
-
-const tabs = [
-    { text: '热门作品', id: 0 },
-    { text: '歌手介绍', id: 1 },
-];
 
 const Singer = ({ state }) => {
     if (!state) {
         return <Loading />;
     }
-    const [activeTab, setActive] = useState(0);
 
     return (
         <div className={style.singer}>
             <Header data={state.header} />
-            <Tabs tabs={tabs} activeTab={activeTab} setActive={setActive} />
-            {activeTab === 0 &&
-                <SongList data={state.songs} />
-            }
-            {activeTab === 1 &&
-                <Introduction data={state.intro} />
-            }
+            <Tabs>
+                <Tabs.Pane key="0" text="热门作品">
+                    <SongList data={state.songs} />
+                </Tabs.Pane>
+                <Tabs.Pane key="1" text="歌手介绍">
+                    <Introduction data={state.intro} />
+                </Tabs.Pane>
+            </Tabs>
         </div>
     );
 }
