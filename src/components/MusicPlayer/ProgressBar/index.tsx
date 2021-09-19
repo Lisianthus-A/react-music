fa897b91683fbbf61ab02876edd710f3d213c544
Lisianthus-A <lisianthus.zeng@gmail.com>
2021-09-20 02:54:00 +0800

    fix: fix bug

--- src/components/MusicPlayer/ProgressBar/index.tsx
+++ src/components/MusicPlayer/ProgressBar/index.tsx
@@ -47,7 +47,13 @@ function ProgressBar({ playingItem, currentTime, setCurrentTime }: Props) {
         const width = window.innerWidth < 1000 ? 1000 : window.innerWidth;
         const percent = e.clientX / width;
 
-        const offset = percent * duration;
+        let offset = percent * duration;
+        // 越界处理
+        if (offset < 0) {
+            offset = 0;
+        } else if (offset > duration) {
+            offset = duration;
+        }
         setCurrentTime(offset);
         playSong(playingItem, offset);
     }, [playingItem]);
@@ -55,7 +61,13 @@ function ProgressBar({ playingItem, currentTime, setCurrentTime }: Props) {
     // 鼠标移动
     const handleMouseMove = useCallback((e) => {
         const width = window.innerWidth < 1000 ? 1000 : window.innerWidth;
-        const percent = e.clientX / width;
+        let percent = e.clientX / width;
+        // 越界处理
+        if (percent > 1) {
+            percent = 1;
+        } else if (percent < 0) {
+            percent = 0;
+        }
         railRef.current.style.width = `${percent * 100}%`;
         tipRef.current.textContent = convertTime(percent * duration);
     }, [playingItem]);
