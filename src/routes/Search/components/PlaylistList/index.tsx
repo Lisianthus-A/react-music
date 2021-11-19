c61bd8bc6b2291c78875aa5de0d64a3132898129
Lisianthus-A <lisianthus.zeng@gmail.com>
2021-11-19 15:57:20 +0800

    style: fix code style

--- src/routes/Search/components/PlaylistList/index.tsx
+++ src/routes/Search/components/PlaylistList/index.tsx
@@ -2,27 +2,27 @@ import React from 'react';
 import LinkImageList from 'Components/LinkImageList';
 
 interface Props {
-  data: {
-    id: number;
-    name: string;
-    // description: string;
-    picUrl: string;
-  }[];
+    data: {
+        id: number;
+        name: string;
+        // description: string;
+        picUrl: string;
+    }[];
 }
 
 function PlaylistList({ data }: Props) {
-  return (
-    <LinkImageList>
-      {data.map(({ id, name, picUrl }) =>
-        <LinkImageList.Item
-          key={id}
-          linkTo={`/Playlist?id=${id}`}
-          src={picUrl}
-          text={name}
-        />
-      )}
-    </LinkImageList>
-  );
+    return (
+        <LinkImageList>
+            {data.map(({ id, name, picUrl }) =>
+                <LinkImageList.Item
+                    key={id}
+                    linkTo={`/Playlist?id=${id}`}
+                    src={picUrl}
+                    text={name}
+                />
+            )}
+        </LinkImageList>
+    );
 }
 
 export default PlaylistList;
