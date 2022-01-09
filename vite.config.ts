dfeb867ddc1ff6e4935850454fe9d05fe2a490be
Lisianthus-A <lisianthus.zeng@gmail.com>
2022-01-09 15:44:14 +0800

    fix: fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
+import process from 'process';
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: isDev ? "/" : '/react-music/',
     server: {
         port: 4000
     },
