7be3592a06bc68e69a81115d8d6d73c602888b57
Lisianthus-A <Lisianthus-A@users.noreply.github.com>
2022-02-11 09:01:25 +0000

    Automated deployment: Fri Feb 11 09:01:25 UTC 2022 0c6f7016b41720b8a7b6e938659abf98d79727ee

---
+++ assets/index.5a87a707.js
@@ -0,0 +1,1 @@
+import{u as r}from"./index.1f299675.js";import{j as s,b as d}from"./vendor.47a03c39.js";const i=()=>{const e=r("id");return s("div",{className:"video",children:["Video Id=",e]})};var o=i,t=()=>d(o,{});export{t as default};
