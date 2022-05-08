329e97256485b4e0be50a7acb448b2ca37f371ba
Lisianthus-A <lisianthus.zeng@gmail.com>
2022-05-09 00:06:07 +0800

    feat: prompt when request error

--- src/apis/apiBase.ts
+++ src/apis/apiBase.ts
@@ -1,30 +1,37 @@
-import axios from 'axios';
-import type { AxiosRequestConfig } from 'axios';
+import { message } from "antd";
+import axios from "axios";
+import type { AxiosRequestConfig } from "axios";
 
 // 服务器地址
-// const baseUrl = 'http://127.0.0.1:4101';
+// const baseUrl = "http://127.0.0.1:4101";
 const baseUrl = 'http://124.220.165.139';
 
 //请求携带的参数
 const baseParams: AxiosRequestConfig = {
-    method: 'GET',
+    method: "GET",
     headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
+        "Content-Type": "application/json",
+        Accept: "application/json",
     },
-    withCredentials: true
+    withCredentials: true,
 };
 
 const ajax = async <T = any>(
     url: string,
-    params: Omit<AxiosRequestConfig, 'url'> = {}
+    params: Omit<AxiosRequestConfig, "url"> = {}
 ): Promise<T> => {
     const response = await axios({
         url: baseUrl + url,
         ...baseParams,
-        ...params
+        ...params,
+    }).catch(() => {
+        message.error("请求失败");
+        return null;
     });
-    return response.data;
-}
+    if (response) {
+        return response.data;
+    }
+    return Promise.reject(null);
+};
 
 export default ajax;
