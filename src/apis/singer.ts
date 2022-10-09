75cc4ab19d6cca5ae370ae6b0062d6ba29f0abc4
Lisianthus-A <lisianthus.zeng@gmail.com>
2022-10-09 09:20:07 +0800

    Update singer.ts

--- src/apis/singer.ts
+++ src/apis/singer.ts
@@ -14,8 +14,16 @@ export const singerInfo = (id: number | string) => {
 /**
  * 歌手介绍
  * @param id 歌手 id
- * @returns 
  */
 export const singerDesc = (id: number | string) => {
     return ajax(`/artist/desc?id=${id}`);
+}
+
+/**
+ * 获取歌手专辑
+ * @param id 歌手 id
+ * @param offset 偏移
+ */
+export const singerAlbum = (id: number | string, offset: number = 0) => {
+    return ajax(`/artist/album?id=${id}&limit=30&offset=${offset}`);
 }
